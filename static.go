f8b6c3d1d1d2ec01f9ef95d09431e3e4b78fea60
cybe42 <usercybe@gmail.com>
2022-05-31 17:19:00 +0100

    Comment some code.

--- static.go
+++ static.go
@@ -11,17 +11,12 @@ import (
 //go:embed static/*
 var static embed.FS
 
-/*var (
-	Index     string
-	Bootstrap string
-)*/ // No need to be global for now
-
 type fileHandler struct {
 	engine *gin.Engine
 }
 
 const (
-	defaultFileType = "text/plain; charset=utf-8"
+	defaultFileType = "text/plain; charset=utf-8" // The default file type if we fail to parse from extension.
 )
 
 func getFileType(filePath string) string {
@@ -32,7 +27,7 @@ func getFileType(filePath string) string {
 	} else {
 		var fileType string = mime.TypeByExtension("." + fileSplitted[splittedLength-1]) // get last element from array
 		if fileType == "" {
-			return defaultFileType
+			return defaultFileType // return file type if we could not find file extension
 		} else {
 			return fileType
 		}
@@ -46,13 +41,13 @@ func (handler *fileHandler) addFileHandle(path string, filePath string) {
 	handler.engine.GET(path, func(c *gin.Context) {
 		c.Header("Content-Type", getFileType(filePath))
 		c.String(200, fileContent)
-	})
+	}) // handling static file
 }
 
 func Static(r *gin.Engine) {
 	var filehandler fileHandler = fileHandler{engine: r}
 	staticDir, _ := static.ReadDir("static")
-	for _, file := range staticDir {
+	for _, file := range staticDir { // looping through all the embedded files
 		var fileName string = file.Name()
 		if fileName != "index.html" {
 			filehandler.addFileHandle("/"+fileName, "static/"+fileName)
