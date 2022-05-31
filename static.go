708a5013a4d804675ea3dfded0a71c899d7ef2fb
cybe42 <usercybe@gmail.com>
2022-05-31 17:57:57 +0100

    changing handle based on file extension.

--- static.go
+++ static.go
@@ -19,18 +19,23 @@ const (
 	defaultFileType = "text/plain; charset=utf-8" // The default file type if we fail to parse from extension.
 )
 
-func getFileType(filePath string) string {
+func getFileExtension(filePath string) string {
 	var fileSplitted []string = strings.Split(filePath, ".")
 	var splittedLength int = len(fileSplitted)
 	if splittedLength < 2 {
-		return defaultFileType
+		return ""
 	} else {
-		var fileType string = mime.TypeByExtension("." + fileSplitted[splittedLength-1]) // get last element from array
-		if fileType == "" {
-			return defaultFileType // return file type if we could not find file extension
-		} else {
-			return fileType
-		}
+		return "." + fileSplitted[splittedLength-1] // get last element from array
+	}
+}
+
+func getFileType(filePath string) string {
+	var fileExtension string = getFileExtension(filePath)
+	var fileType string = mime.TypeByExtension(fileExtension)
+	if fileType == "" {
+		return defaultFileType // return file type if we could not find file extension
+	} else {
+		return fileType
 	}
 }
 
@@ -49,10 +54,12 @@ func Static(r *gin.Engine) {
 	staticDir, _ := static.ReadDir("static")
 	for _, file := range staticDir { // looping through all the embedded files
 		var fileName string = file.Name()
-		if fileName != "index.html" {
-			filehandler.addFileHandle("/"+fileName, "static/"+fileName)
-		} else {
+		if fileName == "index.html" {
 			filehandler.addFileHandle("/", "static/"+fileName)
+		} else if getFileExtension(fileName) == ".html" { // remove .html from path
+			filehandler.addFileHandle("/"+fileName[:len(fileName)-len(".html")], "static/"+fileName)
+		} else { // else just serve file regularly
+			filehandler.addFileHandle("/"+fileName, "static/"+fileName)
 		}
 	}
 }
